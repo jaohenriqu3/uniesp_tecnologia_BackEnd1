3f5e7542ca5fbffe6f433025fbcb66c4170669c3
João Henrique Medeiros de Lucena <joaohenriqu3@academicos.com>
2023-09-08 15:09:36 -0300

    Primeiro commit

---
+++ src/main/java/br/edu/uniesp/projetoBackEnd1/ProjetoBackEnd1Application.java
@@ -0,0 +1,13 @@
+package br.edu.uniesp.projetoBackEnd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackEnd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackEnd1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/uniesp/projetoBackEnd1/ProjetoBackEnd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.uniesp.projetoBackEnd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackEnd1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
